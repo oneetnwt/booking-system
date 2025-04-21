477b27a6e4a762470cfb5755738a46158b351a8d
Jeff Ivan Mayor <jeffivanbiosanomayor@gmail.com>
2025-04-21 23:28:39 +0800

    Finished: Home.php page

--- index.php
+++ index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <link rel="icon" href="./assets/K&ALogo.png">
-    <title>K&A Natural Spring Resort | Book Here</title>
-</head>
-
-<body>
-    <header>
-        <img src="./assets/K&A.png" alt="K&A Natural Spring Resort Logo">
-        <div id="navlink">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Accomodation</a></li>
-                <li><a href="#">Amenities</a></li>
-                <li><a href="#">FAQs</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-            <a id="book_now">Book Now</a>
-        </div>
-    </header>
-    <main>
-        <div id="hero_section">
-            <h1>Escape the Ordinary,<br>Discover Paradise.<br>Book Your Stay Today!</h1>
-            <p>Discover a Haven of Peace, Where Every Moment is an<br>Adventure. Unwind, Refresh, and Reconnect with<br>
-                Nature’s Beauty.</p>
-            <button>Book Now!</button>
-        </div>
-        <div id="quote">
-            <q>Experience the unforgettable happiness with your family and<br>friends at K&A Natural Spring
-                Resort</q>
-        </div>
-    </main>
-</body>
-
-</html>
+<?php
+header("Location: pages/home.php");
+exit();
+?>
