ac202de78a81a933d524b0cbc3d32bcac2184bf0
Jeff Ivan Mayor <jeffivanbiosanomayor@gmail.com>
2025-04-29 20:11:44 +0800

    Added admin panel

---
+++ admin/dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="styles/admin.styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <img src="../assets/K&ALogo.png" alt="" class="circle-logo">
+            <img src="../assets/K&A.png" alt="" class="logo-text">
+        </div>
+        <a href="">
+            Log out
+            <span class="mdi mdi-logout"></span>
+        </a>
+    </header>
+</body>
+
+</html>
