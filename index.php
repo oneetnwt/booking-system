aca4cc80842fc7eabe85b00d25c770eac97eaf43
Jeff Ivan Mayor <jeffivanbiosanomayor@gmail.com>
2025-04-30 19:51:39 +0800

    Changes default file location

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-if (isset($_COOKIE['token'])) {
-    header("Location: home/home.php");
-    exit();
-} else {
-    header("Location: auth/login.php");
-    exit();
-}
+header("Location: home/home.php");
+exit();
+?>
